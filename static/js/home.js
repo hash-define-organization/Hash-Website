e9e3a36d8254515561587d4c3f4fe471b0d76139
WINOFFRG <WINOFFRG @users.noreply.github.com>
2021-11-28 20:40:18 +0000

    Building latest Update - Github Actions

---
+++ static/js/home.js
@@ -0,0 +1,63 @@
+class NotificationManager {
+
+    constructor() {
+    }
+
+    static start(delay) {
+
+    }
+}
+
+
+function notificationHandler() {
+
+
+
+    const notification = document.querySelector('.notification-dismiss');
+
+    notification.addEventListener('click', () => {
+        const notification = document.querySelector('.notification__wrapper');
+        notification.style.display = "none";    
+    });
+}
+
+function emailHandler() {
+
+    function validateEmail(email) {
+        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        return re.test(email);
+    }
+
+    const submitBtn = document.querySelector('.subscribe__form--submit');
+    
+    submitBtn.addEventListener('click', () => {
+        const emailInput = document.querySelector('.subscribe__form--email').value;
+        const messageBox = document.querySelector('.alert-message');
+
+        let submitMessage = messageBox.children[0];
+
+        submitMessage.parentElement.style.display = "block";
+
+        if(validateEmail(emailInput))
+        {
+            submitMessage.innerHTML = "🎉 Thanks for subscribing!";
+        }
+        else
+        {
+            submitMessage.innerHTML = "🙅‍♀️ Please enter a valid email address";
+        }
+
+        // setTimeout(() => {
+        //     submitMessage.style.animation = "fadeOut 0.5s ease-in-out !important";
+        //     submitMessage.parentElement.style.display = "none";
+        // }
+        // , 2000);
+    }); 
+}
+
+function init() {
+    console.log("Home page loaded");
+};
+
+setTimeout(init, 5000);
+emailHandler();
