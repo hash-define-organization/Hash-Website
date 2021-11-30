bbb2abd27615e12f80b9353b87ad05a806229cde
winoffrg <rohangupta1528@gmail.com>
2021-11-30 23:31:59 +0530

    Saved

--- src/js/home.js
+++ src/js/home.js
@@ -1,20 +1,66 @@
+// const dbBaseUrl = 'https://raw.githubusercontent.com/hash-define-organization/website-update/main/database/';
+
+import myObj from './constants.js';
 class NotificationManager {
+
+    #notificationHandler = {};
 
     constructor() {
         this.notificationSound = new Audio('/assets/sounds/notification.mp3');
+
+        notificationSound();
+        showNotification();
     }
 
-    playSound() {
-        this.notificationSound.play();
+    notificationSound () {
+        const audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', '/assets/sounds/notification.mp3');
+        this.#notificationHandler.sound = audioElement;
     }
 
-    static notificationHandler(delay) {
-            setTimeout();
+    showNotification() {
+        this.#notificationHandler.sound.play();
+    }
+
+    async setNotification() {
+        try {
+            this.#notificationHandler.data = await this.fetchNotification();
+            console.log(this.#notificationHandler);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static setInstance(instance) {
+        this.#notificationHandler.instance = instance;
+        console.log(this.#notificationHandler);
+    }
+
+    async fetchNotification() {
+        try {
+            return await (await fetch(`${dbBaseUrl}notifications.json`)).json();
+        } catch (error) {
+            console.log("Failed to check for Notifications");
+        }
+    }
+
+    //show notif
+
+    //dismiss hotification
+
+
+    // Start Notification Manager after some time on Website
+
+    static initNotificationHandler() {
+            setTimeout(() => {
+                NotificationManager.setInstance(new NotificationManager());
+                console.dir(this);
+            }, 0);
     }
 }
 
-const notify = new NotificationManager();
-setTimeout(notify.playSound, 3000);
+NotificationManager.initNotificationHandler();
+
 
 function notificationHandler() {
 
