be0f9bb7bbf8d5e1b5ce7334609bb3fc91248afe
winoffrg <rohangupta1528@gmail.com>
2021-12-11 22:51:39 +0530

    Working on Router v2

--- src/js/router.js
+++ src/js/router.js
@@ -1,3 +1,4 @@
+/*
 class Router {
 
     rootDiv = null;
@@ -28,11 +29,25 @@ class Router {
         path = !path ? window.location.pathname : path;
 
         if(path === '/') {
-            const res = await this.getPage('home');
-            this.routes[path] = res;
+            this.routes[path] = await this.getPage('home');
         }
         else {
-            this.routes[path] = await this.getPage(path);
+            const pageData = await this.getPage(path);
+
+            if(!pageData) {
+                const page404 = await this.getPage('404');
+                window.history.pushState(
+                    {},
+                    pathName,
+                    `${pathName}`,
+                  );
+        
+                this.rootDiv.innerHTML = page404;
+                return;
+            }
+            else {
+                this.routes[path] = pageData;
+            }
         }
 
         this.navigation(path);
@@ -64,3 +79,69 @@ class Router {
 
 const pageRouter = new Router();
 window.pageRouter = pageRouter;
+
+*/
+
+class Router {
+
+    routes = null;
+    globalTitle = null;
+    views = null;
+
+    constructor() {
+
+        this.routes = setRoutes();
+        this.views = setViews();
+        this.globalTitle = 'Hash Define';
+
+    }
+
+    async render() {
+
+
+    }
+
+    setRoutes() {
+        return {
+            '/': {
+                view: 'home',
+                title: `${this.globalTitle}`,
+            },
+            'events': {
+                view: 'events',
+                title: `${this.globalTitle} - Events`,
+            },
+            'team': {
+                view: 'team',
+                title: `${this.globalTitle} - Our Team`,
+            },
+            'about': {
+                view: 'about',
+                title: `${this.globalTitle} - About Us`,
+            },
+            '404': {
+                view: '404',
+                title: `404 - Page Not Found`,
+            }
+        }
+    }
+   
+    setViews() {
+        let views = {};
+
+        Object.keys(this.routes).forEach( route => {
+            views[route] = '';
+        });
+
+        return views;
+    }
+
+    async getView(name) {
+        try {
+            const page = await fetch(`/${name}.html`);
+            return page.text();
+        } catch (error) {
+            console.log(`${name}.html - No Such Page View Found!`);
+        }
+    }
+};
